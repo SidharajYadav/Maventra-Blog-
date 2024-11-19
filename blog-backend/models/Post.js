d1bcbf51520e85601b482d263dcba7002b51e4cc
SiddharajYadav <sidharajyadav157@gmail.com>
2024-11-19 16:57:30 +0530

    Update Post.js

--- blog-backend/models/Post.js
+++ blog-backend/models/Post.js
@@ -1,15 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const PostSchema = new mongoose.Schema({
-//   title: { type: String, required: true },
-//   subtitle: { type: String },
-//   tags: { type: [String] },
-//   content: { type: String, required: true },
-//   createdAt: { type: Date, default: Date.now },
-// });
-
-// module.exports = mongoose.model("Post", PostSchema);
-
 
 const mongoose = require("mongoose");
 
